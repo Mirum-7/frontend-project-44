a7501e4b0777bb6f639cf1f163201e1c77a8cce1
Грязев Эдуард <mirum7@icloud.com>
2023-04-02 19:50:13 +0300

    patch

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -6,7 +6,6 @@ import {
 import game from '../index.js';
 
 const ruleMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
-const getQuestion = () => getRandomNumber();
 const getResult = (question) => booleanToWord(isEven(question));
 
-game(ruleMessage, getQuestion, getResult);
+game(ruleMessage, getRandomNumber, getResult);
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,7 +6,6 @@ import {
 import game from '../index.js';
 
 const ruleMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const getQuestion = () => getRandomNumber();
 const getResult = (question) => booleanToWord(isPrime(question));
 
-game(ruleMessage, getQuestion, getResult);
+game(ruleMessage, getRandomNumber, getResult);
