63f11ef879e47b36d7e152ae06f8510c95b1922a
Грязев Эдуард <mirum7@icloud.com>
2023-04-02 16:36:20 +0300

    update brain gcd

--- src/cli.js
+++ src/cli.js
@@ -18,3 +18,25 @@ export const booleanToWord = (bool) => {
   }
 	return 'no';
 };
+
+export const getGcd = (num1, num2) => {
+	const minNum = Math.min(num1, num2);
+	const maxNum = Math.max(num1, num2);
+
+	let maxDivisor = 0;
+
+	for(let i = 1; i<= Math.sqrt(minNum) + 1; i++){
+		if (minNum % i === 0) {
+			const oppositeDivisor = minNum / i
+			if (maxNum % oppositeDivisor === 0 && oppositeDivisor > maxDivisor) {
+				maxDivisor = oppositeDivisor
+				continue
+			}
+			if (maxNum % i === 0 && i > maxDivisor){
+				maxDivisor = i;
+			}
+		}
+	}
+
+	return maxDivisor
+};
